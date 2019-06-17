632685cbe442e240a62192f5c833b2b6b6aef19e
Peter Tribble <peter.tribble@gmail.com>
2019-06-17 09:18:09 +0100

    haplo-example/haplo-app.sh needs to match that in haplo-app

--- haplo-example/haplo-app.sh
+++ haplo-example/haplo-app.sh
@@ -27,6 +27,12 @@ fi
 . app.values
 
 #
+# default application template and ID
+#
+APPTEMPLATE="sme"
+APPID="4000"
+
+#
 # check the variables are set
 #
 if [ -z "$APPURL" ]; then
@@ -48,6 +54,24 @@ fi
 if [ -z "$APPUPASS" ]; then
     echo "APPUPASS unset"
     exit 1
+fi
+
+#
+# If we have a set of plugins specified then load those
+# use the minimal template in that case
+# The plugins will be in subdirectories of /opt/haplo/sample-plugins
+# and we copy the named subdirectory to /haplo/plugins
+#
+if [ -n "$APPPLUGIN" ]; then
+    APPTEMPLATE="minimal"
+    if [ -d /opt/haplo/sample-plugins/${APPPLUGIN} ]; then
+	sudo mkdir -p /haplo/plugins
+	sudo cp -r /opt/haplo/sample-plugins/${APPPLUGIN} /haplo/plugins
+	sudo chown -hR haplo:haplo /haplo/plugins
+    else
+	echo "Invalid plugin set $APPPLUGIN"
+	exit 1
+    fi
 fi
 
 #
@@ -87,22 +111,33 @@ fi
 # the app, and postgres, must be running
 #
 sudo /etc/init.d/postgresql start
+echo " *** Waiting 5s for database to start ***"
 sleep 5
 sudo su haplo -c "/opt/haplo/deploy/haplo.rc do_start"
+echo " *** Waiting 30s for haplo server to start ***"
 sleep 30
 
 #
 # initialize the app
+# if plugins specified, install those too
 #
-if [ ! -d /haplo/files/4000/ ]; then
+if [ ! -d /haplo/files/${APPID}/ ]; then
     rm -f /tmp/haplo-appinit.sh
     cat > /tmp/haplo-appinit.sh <<EOF
 #!/bin/sh
 cd /opt/haplo
-db/init_app.sh haplo $APPURL "${APPNAME}" sme 4000
+db/init_app.sh haplo $APPURL "${APPNAME}" ${APPTEMPLATE} ${APPID}
 sleep 1
 db/create_app_user.sh $APPURL "${APPUNAME}" ${APPUMAIL} ${APPUPASS}
 EOF
+    if [ -n "$APPPLUGIN" ]; then
+	cat >> /tmp/haplo-appinit.sh <<EOF
+sleep 1
+echo " *** Installing $APPPLUGIN plugins ***"
+script/runner "KApp.in_application(${APPID}) {KPlugin.install_plugin('${APPPLUGIN}')}"
+echo " *** Installed $APPPLUGIN plugins ***"
+EOF
+    fi
     chmod a+x /tmp/haplo-appinit.sh
     sudo su haplo -c "/tmp/haplo-appinit.sh"
     rm -f /tmp/haplo-appinit.sh
